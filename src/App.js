fd7ee995ec0f217510a011bf393b1bbeba1541da
Raihan <raihanislam.cse@gmail.com>
2022-01-16 19:07:22 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import  { Box, Typography, withStyles } from '@material-ui/core';
+import logo from './images/COVID19.jpg';
+import { fetchData } from './service/api';
+import Cards from './components/Cards';
+import Countries from './components/Countries';
+import Chart from './components/Chart';
+const style = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+
+  header: {
+    background: '#45df13',
+    width: 400,
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+    color: '#fff'
+  },
+  lastUpdated: {
+    color: '#777',
+    fontSize: 12
+  }
+  
+}
+ class App extends Component {
+  state = {
+    data: {},
+    country: ''
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData})
+    console.log(fetchedData);
+  }
+
+  handleCountryChange = async(country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({data: fetchedData, country: country})
+    console.log(fetchedData);
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <Box className={this.props.classes.container}>
+        <Box className={this.props.classes.header}>COVID-19 CORONAVIRUS PANDEMIC</Box>
+        <Typography className={this.props.classes.lastUpdated}>Last Updated: {data.lastUpdate && new Date(data.lastUpdate).toDateString()}</Typography>
+        <img style={{width:350}} src={logo} alt="covid"/>
+        <Cards data={data}/>
+        <Countries handleCountryChange={this.handleCountryChange}/>
+        {/* <Chart data={data} /> */}
+      </Box>
+    )
+  }
+}
+
+export default withStyles(style)(App)
